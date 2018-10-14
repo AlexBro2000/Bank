13b0ae547cf5053484940e922b1ef4c52b5e75e7
Mixan Panama <mixanpanama@gmail.com>
2018-10-14 13:37:41 +0300

    The loging to file was implemented

--- src/com/bank/model/service/LogService.java
+++ src/com/bank/model/service/LogService.java
@@ -1,11 +1,15 @@
 package com.bank.model.service;
 
+import com.bank.model.service.validator.FileService;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LogService {
-    
+
     private static LogService logService;
+    private FileService fileService = FileService.getInstance();
 
     private LogService() {
     }
@@ -26,8 +30,15 @@ public class LogService {
     }
 
     private void log(String modePrefix, String message){
-        System.out.println(LocalDateTime.now().format(DateTimeFormatter.
+        String fullMessage = LocalDateTime.now().format(DateTimeFormatter.
                 ofPattern("yyyy-MM-dd HH:mm:ss")) + " " +
-                modePrefix + " " + message);
+                modePrefix + " " + message;
+        System.out.println(fullMessage);
+
+        try {
+            fileService.write(fullMessage, "/log.txt");
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
     }
 }
